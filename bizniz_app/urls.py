2575936e388f4d3b1a73935b8c1be61a54f4cf4d
Vincent <service@fruitdatakings.com>
2023-05-01 07:16:32 +0200

    twetch page initial

--- bizniz_app/urls.py
+++ bizniz_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('twetch/', views.show_images, name='twetch'),
+    path('twetch/random/', views.random_images, name='random_images'),
+    path('twetch/next/<str:start_index>/', views.next_images, name='next_images'),
+    path('previous_images/<int:start_index>/', views.previous_images, name='previous_images'),
 ]
--- bizniz_app/views.py
+++ bizniz_app/views.py
@@ -1,5 +1,8 @@
 import os
+from random import shuffle
 import csv
+import json
+import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import CsvEntry
@@ -42,3 +45,64 @@ def card(request):
     import_csv()  # Import the CSV data
     entries = CsvEntry.objects.all().order_by('-createdAt')
     return render(request, 'card.html', {'entries': entries})
+
+def read_csv_file(file_path):
+    with open(file_path, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        rows = [row for row in csv_reader]
+
+    # Sort rows by 'createdAt' in descending order
+    rows.sort(key=lambda x: x['timestamp'], reverse=True)
+
+    images = []
+    for row in rows:
+        if 'files' in row and row['files']:
+            files = json.loads(row['files'].replace("'", '"'))
+            for file in files:
+                images.append(file)
+    return images
+
+
+def show_images(request):
+    csv_file_path = 'bizniz_app/data/twetch_grid.csv'
+    images = read_csv_file(csv_file_path)
+    next_start_index = 50
+    context = {'images': images[:50], 'next_start_index': next_start_index, 'previous_start_index': 0}
+    return render(request, 'twetch.html', context)
+
+def random_images(request):
+    csv_file_path = 'bizniz_app/data/twetch_grid.csv'
+    images = read_csv_file(csv_file_path)
+    random.shuffle(images)  # Use random.shuffle to randomize the list
+    images = images[:50]
+    next_start_index = 50
+    context = {'images': images, 'next_start_index': next_start_index, 'previous_start_index': 0}
+    return render(request, 'twetch.html', context)
+
+
+def next_images(request, start_index):
+    start_index = int(start_index)
+    csv_file_path = 'bizniz_app/data/twetch_grid.csv'
+    images = read_csv_file(csv_file_path)
+    end_index = start_index + 50
+    images = images[start_index:end_index]
+    next_start_index = end_index
+    context = {'images': images, 'next_start_index': next_start_index, 'previous_start_index': start_index - 50 if start_index - 50 >= 0 else 0}
+    return render(request, 'twetch.html', context)
+
+def previous_images(request, start_index):
+    start_index = int(start_index)
+    if start_index - 50 < 0:
+        previous_start_index = 0
+    else:
+        previous_start_index = start_index - 50
+
+    csv_file_path = 'bizniz_app/data/twetch_grid.csv'
+    images = read_csv_file(csv_file_path)
+    images = images[previous_start_index:start_index]
+    
+    return render(request, 'twetch.html', {
+        'images': images,
+        'previous_start_index': previous_start_index - 50 if previous_start_index - 50 >= 0 else 0,
+        'next_start_index': start_index,
+    })
